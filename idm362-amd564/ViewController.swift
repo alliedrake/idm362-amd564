2e89efdef7cc7af21f3a4b6555fe4187b5028eec
alliedrake <amd564@drexel.edu>
2023-02-09 12:01:23 -0500

    In-class work

--- idm362-amd564/ViewController.swift
+++ idm362-amd564/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    
+    var vCounter:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayLabel.textColor = UIColor(named: "Prince")
     }
 
 
+    
+    @IBAction func addBtn(_ sender: Any) {
+        vCounter += 1
+        displayLabel.text = String(vCounter)
+    }
+    
+    
+    @IBAction func touchMe(_ sender: Any) {
+        displayLabel.text = "Well Hello there \(nameText.text!)"
+    }
+    
+// Disable Keyboard when user touches elsewhere
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    
 }
 
